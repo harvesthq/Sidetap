23ef3a20875f346be684b589b368bba24811d163
Joschka <joschka@kintscher.me>
2012-06-25 14:23:14 -0400

    Add preloading to the thumbnail view

--- demo/sleuth/assets/coffee/sleuth.coffee
+++ demo/sleuth/assets/coffee/sleuth.coffee
@@ -19,34 +19,49 @@ $(document).ready () ->
   $.getJSON 'assets/json/images.json', (images) ->
 
     # show the default section
-    render_thumbnails(st.stp_nav.find('a.selected').text(), images)
+    show_thumbnails(st.stp_nav.find('a.selected').text(), images)
 
     st.stp_nav.find('nav a').click () ->
       $(this).addClass('selected').siblings().removeClass('selected')
       st.toggle_nav()
 
-      render_thumbnails($(this).text(), images)
+      show_thumbnails($(this).text(), images)
       no
 
 
-  render_thumbnails = (section, images) ->
-    thumbnails.empty()
-
-    thumbnails.append $("""
-      <li#{if ( i + 1 ) % 4 is 0 then ' class="right"' else ''}>
-        <a href='javascript:void(0)'>
-          <img src='#{img.url_s}' alt='#{i}' />
-        </a>
-      </li>""") for img, i in images[section]
-
+  show_thumbnails = (section, images) ->
+    thumbnails.empty().addClass('loading')
     gallery.find('h1').text(section + ' Bears')
 
+    thumbs = []
+
+    total  = images[section].length
+    loaded = 0
+
+    # preload tumbnails
+    thumbs.push $("<img src='#{img.url_s}' />").load(() ->
+      loaded++
+      render_thumbnails(images[section]) if loaded is total
+    ) for img, i in images[section]
+
+
+  render_thumbnails = (images) ->
+    thumbnails.removeClass 'loading'
+
+    thumbnails.append $("""
+      <li#{if (i + 1) % 4 is 0 then ' class="right"' else ''}>
+        <a href='#'><img src='#{img.url_s}' alt='#{i}' /></a>
+      </li>"""
+    ) for img, i in images
+
     $('#gallery .thumbnails a').click () ->
-      show_image images[section][$(this).find('img').prop('alt')]
+      show_image(images[$(this).find('img').prop('alt')])
 
 
-  show_image = (img) ->
-    detail.find('.stp-content-body img').prop('src', img.url_m)
+  show_image = (img) =>
+    detail.find('.stp-content-body img').replaceWith($("<img src='#{img.url_m}' />"))
+
+    # show meta info for image
     detail.find('cite').html(img.title)
     detail.find('[rel="author"]')
           .prop('href', "http://flickr.com/photos/#{img.owner}/#{img.id}")
--- demo/sleuth/assets/js/sleuth.js
+++ demo/sleuth/assets/js/sleuth.js
@@ -2,7 +2,8 @@
 (function() {
 
   $(document).ready(function() {
-    var about, detail, gallery, render_thumbnails, show_image, st, thumbnails;
+    var about, detail, gallery, render_thumbnails, show_image, show_thumbnails, st, thumbnails,
+      _this = this;
     st = sidetap();
     gallery = $('#gallery');
     thumbnails = gallery.find('.thumbnails');
@@ -25,29 +26,47 @@
       });
     });
     $.getJSON('assets/json/images.json', function(images) {
-      render_thumbnails(st.stp_nav.find('a.selected').text(), images);
+      show_thumbnails(st.stp_nav.find('a.selected').text(), images);
       return st.stp_nav.find('nav a').click(function() {
         $(this).addClass('selected').siblings().removeClass('selected');
         st.toggle_nav();
-        render_thumbnails($(this).text(), images);
+        show_thumbnails($(this).text(), images);
         return false;
       });
     });
-    render_thumbnails = function(section, images) {
-      var i, img, _i, _len, _ref;
-      thumbnails.empty();
+    show_thumbnails = function(section, images) {
+      var i, img, loaded, thumbs, total, _i, _len, _ref, _results;
+      thumbnails.empty().addClass('loading');
+      gallery.find('h1').text(section + ' Bears');
+      thumbs = [];
+      total = images[section].length;
+      loaded = 0;
       _ref = images[section];
+      _results = [];
       for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
         img = _ref[i];
-        thumbnails.append($("<li" + ((i + 1) % 4 === 0 ? ' class="right"' : '') + ">\n  <a href='javascript:void(0)'>\n    <img src='" + img.url_s + "' alt='" + i + "' />\n  </a>\n</li>"));
+        _results.push(thumbs.push($("<img src='" + img.url_s + "' />").load(function() {
+          loaded++;
+          if (loaded === total) {
+            return render_thumbnails(images[section]);
+          }
+        })));
       }
-      gallery.find('h1').text(section + ' Bears');
+      return _results;
+    };
+    render_thumbnails = function(images) {
+      var i, img, _i, _len;
+      thumbnails.removeClass('loading');
+      for (i = _i = 0, _len = images.length; _i < _len; i = ++_i) {
+        img = images[i];
+        thumbnails.append($("<li" + ((i + 1) % 4 === 0 ? ' class="right"' : '') + ">\n  <a href='#'><img src='" + img.url_s + "' alt='" + i + "' /></a>\n</li>"));
+      }
       return $('#gallery .thumbnails a').click(function() {
-        return show_image(images[section][$(this).find('img').prop('alt')]);
+        return show_image(images[$(this).find('img').prop('alt')]);
       });
     };
     return show_image = function(img) {
-      detail.find('.stp-content-body img').prop('src', img.url_m);
+      detail.find('.stp-content-body img').replaceWith($("<img src='" + img.url_m + "' />"));
       detail.find('cite').html(img.title);
       detail.find('[rel="author"]').prop('href', "http://flickr.com/photos/" + img.owner + "/" + img.id).html(img.ownername);
       return st.show_section(detail, {
